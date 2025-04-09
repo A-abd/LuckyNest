55f108a27e4cb207aac69675f07fd58d7fd162a6
A-abd <65906361+A-abd@users.noreply.github.com>
2025-04-09 08:39:06 +0100

    feat: changed db from using sqlite to mysql. also left sqlite db in a folder in the include file as a backup

--- include/db.php
+++ include/db.php
@@ -1,8 +1,32 @@
 <?php
-$dsn = "sqlite:" . __DIR__ . "/luckynest.db";
+$env_path = __DIR__ . "/../.env";
+
+if (file_exists($env_path)) {
+    $env_lines = file($env_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($env_lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($name, $value) = explode('=', $line, 2);
+        $_ENV[trim($name)] = trim($value);
+        putenv(sprintf('%s=%s', trim($name), trim($value)));
+    }
+}
+
+$host = getenv('DB_HOST') ?: 'localhost';
+$dbname = getenv('DB_NAME') ?: 'luckynest';
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: ''; 
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
 try {
-    $conn = new PDO($dsn);
+    $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
